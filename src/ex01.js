a634774f0e1cf48ce5b6c3c572f5cbc1fd2d11ef
yiminwook <grs0416@gmail.com>
2024-08-07 17:48:42 +0900

    #DEV#NEW - helper

--- src/ex01.js
+++ src/ex01.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import gsap from "gsap";
+import Stats from "stats.js";
 
 // const orthographicCamera = new THREE.OrthographicCamera(
 //   -(window.innerWidth / window.innerHeight), //left
@@ -31,32 +32,40 @@ export default function example() {
   // renderer.setClearColor(0x00ff00, 1); // 캔버스 배경 투명하게, hex, opacity
 
   const scene = new THREE.Scene();
+  // scene.background = new THREE.Color("blue");
+  // scene.fog = new THREE.Fog("blue", 8, 12);
 
-  scene.background = new THREE.Color("blue");
-  scene.fog = new THREE.Fog("blue", 8, 12);
+  const axexHelper = new THREE.AxesHelper(3);
+  scene.add(axexHelper);
 
-  const light = new THREE.DirectionalLight(0xffffff, 1);
-  light.position.set(0, 2, 10);
-  scene.add(light);
+  const gridHelper = new THREE.GridHelper(5, 5);
+  scene.add(gridHelper);
 
-  const perspectiveCamera = new THREE.PerspectiveCamera(
+  const stats = new Stats();
+  document.body.appendChild(stats.dom);
+
+  const derectionalLight = new THREE.DirectionalLight("white", 1);
+  derectionalLight.position.set(1, 0, 2);
+  scene.add(derectionalLight);
+
+  const ambientLight = new THREE.AmbientLight("white", 0.5); // 전체적인 조명
+  scene.add(ambientLight);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const meterial = new THREE.MeshStandardMaterial({ color: "seagreen" });
+  const mesh = new THREE.Mesh(geometry, meterial);
+  mesh.position.set(1, 0, 0);
+  scene.add(mesh);
+
+  const camera = new THREE.PerspectiveCamera(
     75, // FOV
     window.innerWidth / window.innerHeight, // Aspect ratio
     0.1, // Near
     1000 // Far
   );
-  perspectiveCamera.position.set(0, 2, 10);
-  scene.add(perspectiveCamera);
-
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const meterial = new THREE.MeshStandardMaterial({ color: "red" });
-
-  const meshs = Array.from({ length: 10 }, (_, i) => i).map((i) => {
-    const mesh = new THREE.Mesh(geometry, meterial);
-    mesh.position.x = i * 2 - 10;
-    scene.add(mesh);
-    return mesh;
-  });
+  camera.position.set(1, 5, 5);
+  camera.lookAt(mesh.position);
+  scene.add(camera);
 
   const clock = new THREE.Clock();
 
@@ -65,30 +74,18 @@ export default function example() {
     // 1초에 360도 회전
     // Math.PI / 180 = 1도
     const delta = clock.getDelta();
-    meshs.forEach((mesh, i) => {
-      gsap.to(mesh.position, {
-        duration: 1, // 1초
-        y: 2 * i,
-        z: 3,
-      });
-    });
-    // console.log(delta);
-    // mesh.rotation.y += delta;
-    // mesh.position.y += delta;
-    // if (mesh.position.y > 3) {
-    //   mesh.position.y = 0;
-    // }
-    renderer.render(scene, perspectiveCamera); // 복수의 카메라를 사용할 수 있음
-    // window.requestAnimationFrame(draw);
+    mesh.rotation.y += delta;
+    renderer.render(scene, camera); // 복수의 카메라를 사용할 수 있음
+    stats.update();
     renderer.setAnimationLoop(draw);
   }
 
   window.addEventListener("resize", () => {
-    perspectiveCamera.aspect = window.innerWidth / window.innerHeight;
-    perspectiveCamera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
-    renderer.render(scene, perspectiveCamera);
+    renderer.render(scene, camera);
   });
 
   draw();
